2cfba37605c1d323a979081e5fa444451bf0fcd4
msennane <msennane@e1r6p4.1337.ma>
2023-08-19 21:30:15 +0100

    Done

--- rush.c
+++ rush.c
@@ -1,51 +1,7 @@
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-	// printing a rectangle of stars with octothorpes at the corners amd pipe characters on the sides
-
-	int i, j, width, height;
-
-	printf("Enter width: ");
-
-	scanf("%d", &width);
-
-	printf("Enter height: ");
-
-	scanf("%d", &height);
-	int j, i;
-
-	for (i = 0; i < height; i++)
-	{
-		for (j = 0; j < width; j++)
-		{
-			if ((i == 0 && j == 0) || (i == 0 && j == width - 1) || (i == height - 1 && j == 0) || (i == height - 1 && j == width - 1))
-			{
-				printf("#");
-			}
-			else if (i == 0 || i == height - 1)
-			{
-				printf("-");
-			}
-			else if (j == 0 || j == width - 1)
-			{
-				printf("|");
-			}
-			else
-			{
-				printf(" ");
-			}
-		}
-		printf("\n");
-	}
-	return 0;
-}
-#include <stdio.h>
-
-int main(void)
-{
-	// printing a rectangle of stars with octothorpes at the corners amd pipe characters on the sides
-
 	int i, j, width, height;
 
 	printf("Enter width: ");
@@ -57,7 +13,6 @@ int main(void)
 	scanf("%d", &height);
 
 	i = 0;
-
 	while (i < height)
 	{
 		j = 0;
@@ -81,8 +36,9 @@ int main(void)
 			}
 			j++;
 		}
-		i++;
 		printf("\n");
+		i++;
 	}
+
 	return 0;
 }
